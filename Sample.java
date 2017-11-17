5236e21fcf2d7fb10bd08e6541f6300d03de3d8b
Devi <devi.subramanian13@gmail.com>
2017-11-17 14:53:10 +0530

    commited3

--- Sample.java
+++ Sample.java
@@ -3,6 +3,6 @@ public class Sample
 public static void main(String[] args)
 {
 System.out.println("Welcome to Altisource Learning Center");
-System.out.println("**********************************************");
+System.out.println("Demo starts");
 }
 }
